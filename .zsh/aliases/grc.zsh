8c48f78ad422303f0c254da7277fd5c0f0974289
ridouchire <ridouchire@gmail.com>
2018-08-31 20:18:08 +0400

    [!] {@324} Aliases: GRC: Log: Alias for log was removed.

--- .zsh/aliases/grc.zsh
+++ .zsh/aliases/grc.zsh
@@ -1,6 +1,5 @@
 if [ -f /usr/bin/grc ]; then
     alias gcc="grc --colour=auto gcc"
-    alias log="grc --colour=auto log"
     alias netstat="grc --colour=auto netstat"
     alias ping="grc --colour=auto ping"
     alias traceroute="grc --colour=auto traceroute"
