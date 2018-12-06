ac3f89b668fb016d284e75691a5e7e153582f5c4
ridouchire <ridouchire@gmail.com>
2018-12-06 17:11:00 +0400

    [!] {@5518} Zsh: Aliases: Work: The name of the container in which the unit tests were run has changed.

--- .zsh/aliases/work.zsh
+++ .zsh/aliases/work.zsh
@@ -1,8 +1,8 @@
 run_unit_test() {
     if [ $1 ] ; then
         echo "Run $1 test"
-        docker exec -it devcart_php_1 bash -c "cd _tools/unit_tests && ../../app/lib/vendor/bin/phpunit ../../$1"
+        docker exec -it dev-cart-php bash -c "cd _tools/unit_tests && ../../app/lib/vendor/bin/phpunit ../../$1"
     else
-        docker exec -it devcart_php_1 bash -c 'cd _tools/unit_tests && ../../app/lib/vendor/bin/phpunit'
+        docker exec -it dev-cart-php bash -c 'cd _tools/unit_tests && ../../app/lib/vendor/bin/phpunit'
     fi
 }
