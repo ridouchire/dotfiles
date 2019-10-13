715f5744ff5c68b8b98ae43c8f833a97573534ed
ridouchire <ridouchire@gmail.com>
2019-10-13 11:19:23 +0400

    [*] ACPI: Actions was fixed && Conf.d: Added new config files with gpsd and ntp client.

--- etc/acpi/actions/lm_ac_adapter.sh
+++ etc/acpi/actions/lm_ac_adapter.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
-DISPLAY=:0.0 su ridouchire -c 'xrandr --output HDMI3 --auto'
-DISPLAY=:0.0 su ridouchire -c 'xrandr --output LVDS1 --off'
+DISPLAY=:0.0 su ridouchire -c 'xrandr --auto'
 
 
 test -f /lib/udev/lmt-udev || exit 0
