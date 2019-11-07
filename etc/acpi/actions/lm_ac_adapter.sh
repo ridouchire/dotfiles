2b9b910737662d909d00c4dd4655a2f5ea5fa7b1
ridouchire <ridouchire@gmail.com>
2019-11-07 09:52:37 +0400

    [*] ACPI: Delete configurations.

--- etc/acpi/actions/lm_ac_adapter.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-DISPLAY=:0.0 su ridouchire -c 'xrandr --auto'
-
-
-test -f /lib/udev/lmt-udev || exit 0
-
-# ac on/offline event handler
-/lib/udev/lmt-udev
--- etc/acpi/actions/lm_lid.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-DISPLAY=:0.0 su ridouchire -c 'i3lock -c 111111'
-
-test -f /lib/udev/lmt-udev || exit 0
-
-# lid button pressed/released event handler
-/lib/udev/lmt-udev
